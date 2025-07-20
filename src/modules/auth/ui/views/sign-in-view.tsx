8c7b993c90da647a7f4b7fed696acd3bbc614dc5
Dumindu Rajapaksha <91795441+Dumidu1212@users.noreply.github.com>
2025-07-21 00:52:57 +0530

    Update src/modules/auth/ui/views/sign-in-view.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -71,7 +71,7 @@ export const SignInView = () => {
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 md:p-8">
               <div className="flex flex-col gap-6">
-                <div className="flrx flex-col items-center text-center">
+                <div className="flex flex-col items-center text-center">
                   <h1 className="text-2xl font-bold">
                     Welcome back
                   </h1>
