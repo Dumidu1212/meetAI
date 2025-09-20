e46daa9f50d0da66ba379edb0319b72407736ed7
dumidu.rajapaksha <91795441+Dumidu1212@users.noreply.github.com>
2025-09-20 20:23:51 +0530

    18: meetings data table

--- src/modules/agents/ui/components/columns.tsx
+++ src/modules/agents/ui/components/columns.tsx
@@ -6,10 +6,10 @@ import { CornerDownRightIcon, VideoIcon } from "lucide-react"
 import { Badge } from "@/components/ui/badge"
 import { GeneratedAvatar } from "@/components/generated-avatar"
 
-import { AgentGetOne } from "../../types"
+import { AgentsGetMany } from "../../types"
 
 
-export const columns: ColumnDef<AgentGetOne>[] = [
+export const columns: ColumnDef<AgentsGetMany[number]>[] = [
   {
     accessorKey: "name",
     header: "Agent Name",
